e85207f24d6bc6488cdf0a32476474c01267d959
ArthurL <arthurlibreloncarvalho@gmail.com>
2020-07-25 10:24:05 -0300

    Comentários adicionados

--- fator.py
+++ fator.py
@@ -1,14 +1,20 @@
 import primo
 def fatorate(number):
+    #lista inicial de primos, incrementada com primo.py
     plist = [2,3,5,7]
+    #divide o número por todos os primos
     for p in plist:
         pwr = 0
+        #enquanto for divisivel:
         while number % p == 0:
+            #divide-se e aumenta o expoente do número em 1
             pwr += 1
             number = number / p
+        #se não for divisivel por esse numero primo
         else:
             if pwr != 0:
                 print(p,'^',pwr)
+        #pede o próximo primo se o número não está completamente dividido
         if p == plist[-1] and number != 1:
             primo.nextprime(plist)
 loop = 'y'
--- primo.py
+++ primo.py
@@ -1,12 +1,17 @@
 def nextprime(plist):
+    #seleciona o maior numero primo da lista
     n = plist[-1]
+    #ativa o loop
     prime = True
-    while prime:
+    while prime == False:
+        #próximo número natural para checar
         n += 1
+        #testa se é primo
         for p in plist:
             if n % p == 0:
                 break
             else:
+                #se for primo, adiciona a lista de primos
                 if p == plist[-1]: plist.append(n); prime = False
     #print(plist)
 
